49679df33dc20b727051f7bc17065a118d97820c
alaabouremoum <alaebouremoum@gmail.com>
2023-04-13 06:53:57 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById('menu'); 
+let close =document.getElementById('close'); 
+let nav =document.getElementById('nav');
+
+ menu.onclick = function(){ 
+    menu.style.visibility ='hidden'; 
+    nav.style.right ='0'; 
+    close.style.visibility ='visibile';
+ }
+ close.onclick =function(){ 
+    menu.style.visibility ="visible" 
+    close.style.visibility ="visible"; 
+    nav.style.right ="-210px"
+ }
